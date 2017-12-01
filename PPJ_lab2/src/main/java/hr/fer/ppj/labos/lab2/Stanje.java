d9c262e93f1adfd82428064a0310138c2443f830
Luka Banović <luka.banovic@fer.hr>
2017-12-01 18:10:58 +0100

    Klasa Stanje za opis stanja DKA

---
+++ PPJ_lab2/src/main/java/hr/fer/ppj/labos/lab2/Stanje.java
@@ -0,0 +1,39 @@
+package hr.fer.ppj.labos.lab2;
+
+import java.util.List;
+import java.util.Map;
+
+import hr.fer.ppj.labos.lab2.EpsilonNKA.LR1Stavka;
+
+public class Stanje {
+	
+	private int index;
+	private List<LR1Stavka> sadrzaj;
+	private Map<String, Integer> prijelazi;
+	
+	public void nadjiPrijelaze(List<Stanje> listaStanja){
+		for (LR1Stavka stavka : sadrzaj) {
+			if(stavka.getPrijelaz() != null){ //ako je stavka potpuna, nema prijelaz, tj prijelaz je null
+				String znakZaPrijelaz = stavka.getPrijelaz().getZnakKojiPokrecePrijelaz();
+				LR1Stavka prelaziU = stavka.getPrijelaz().getSljedecaStavka();
+				for (Stanje stanje : listaStanja) {
+					if(stanje.getSadrzaj().contains(prelaziU)){
+						prijelazi.put(znakZaPrijelaz, stanje.getIndex());
+					}
+				}
+			}
+		}
+	}
+	
+	public int getIndex(){
+		return index;
+	}
+	
+	public List<LR1Stavka> getSadrzaj(){
+		return sadrzaj;
+	}
+	
+	public Map<String, Integer> getPrijelazi(){
+		return prijelazi;
+	}
+}
