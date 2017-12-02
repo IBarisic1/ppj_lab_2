a1314e2fb88f62eb768b8048ca2db5a1be251265
Ivan Barišić <ivan.barisic@fer.hr>
2017-12-02 11:24:17 +0100

    Napravljen razred za tablicu Novo Stanje

---
+++ PPJ_lab2/src/main/java/hr/fer/ppj/labos/lab2/NovoStanje.java
@@ -0,0 +1,29 @@
+package hr.fer.ppj.labos.lab2;
+
+import java.util.List;
+import java.util.Map;
+
+public class NovoStanje {
+	private Map<String, Map<Integer, List<String>>> produkcije;
+	private int[][] tablica;
+	private List<String> nezavrsniZnakovi;
+
+	public NovoStanje(DKA dka, List<String> nezavrsniZnakovi, Map<String, Map<Integer, List<String>>> produkcije) {
+
+		this.produkcije = produkcije;
+		List<Stanje> stanjaDka = dka.getStanja();
+		this.nezavrsniZnakovi = nezavrsniZnakovi;
+
+		tablica = new int[stanjaDka.size()][nezavrsniZnakovi.size()];
+
+		for (Stanje stanje : stanjaDka) {
+			for (Map.Entry<String, Integer> prijelaz : stanje.getPrijelazi().entrySet()) {
+				String znak = prijelaz.getKey();
+				Integer novoStanje = prijelaz.getValue();
+				if (nezavrsniZnakovi.contains(znak))
+					tablica[stanje.getIndex()][nezavrsniZnakovi.indexOf(znak)] = novoStanje;
+			}
+
+		}
+	}
+}
