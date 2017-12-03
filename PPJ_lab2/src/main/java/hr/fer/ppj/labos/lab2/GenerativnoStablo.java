2004bff27879ee56d35ca9f51fb197e746d6163c
Ivan Barišić <ivan.barisic@fer.hr>
2017-12-03 11:51:40 +0100

    Dodao parser za tablicu uniformnih znakova, napravio dio parsiranja ulaznog niza(pomakni, reduciraj, prihvati), jos treba dodati obradu od pogreske, ispravio nacin dodavanja djece unutarnjem cvoru u metodi reduciraj(dodavali su se u obrnutom redoslijedu), dodao racunanje stanja na vrhu stoga u metodi reduciraj

--- PPJ_lab2/src/main/java/hr/fer/ppj/labos/lab2/GenerativnoStablo.java
+++ PPJ_lab2/src/main/java/hr/fer/ppj/labos/lab2/GenerativnoStablo.java
@@ -1,81 +1,134 @@
 package hr.fer.ppj.labos.lab2;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+
+import hr.fer.ppj.labos.lab2.Akcija.Par;
+import hr.fer.ppj.labos.lab2.ParserTabliceUniformnihZnakova.ZapisTabliceUniformnihZnakova;
 
 public class GenerativnoStablo {
 	private Stack<Cvor> stog;
 	private final List<String> nezavrsni;
 	private final List<String> zavrsni;
 	private final List<String> sinkronizacijski;
-	private final Akcija akcija;
-	private final NovoStanje novoStanje;
+	private final Par[][] tablicaAkcija;
+	private final Integer[][] tablicaNovoStanje;
 	private Cvor korijen;
 	private Map<String, Map<Integer, List<String>>> produkcije;
-	
-	public GenerativnoStablo(
-			Akcija akcija, NovoStanje novoStanje, 
-			Map<String, Map<Integer, List<String>>> produkcije) {
-		this.nezavrsni = novoStanje.getNezavrsniZnakovi();
-		this.zavrsni = akcija.getZavrsniIKraj();
-		this.sinkronizacijski = akcija.getSinkronizacijski();
+	private List<ZapisTabliceUniformnihZnakova> uniformniZnakoviUlaznogNiza;
+
+	public GenerativnoStablo(Akcija tablicaAkcija, NovoStanje tablicaNovoStanje,
+			List<ZapisTabliceUniformnihZnakova> uniformniZnakoviUlaznogNiza) {
+		this.nezavrsni = tablicaNovoStanje.getNezavrsniZnakovi();
+		this.zavrsni = tablicaAkcija.getZavrsniIKraj();
+		this.sinkronizacijski = tablicaAkcija.getSinkronizacijski();
 		this.stog = new Stack<>();
-		this.akcija = akcija;
-		this.novoStanje = novoStanje;
-		this.produkcije = produkcije;
+		this.tablicaAkcija = tablicaAkcija.getTablica();
+		this.tablicaNovoStanje = tablicaNovoStanje.getTablica();
+		this.produkcije = tablicaAkcija.getProdukcije();
+		this.uniformniZnakoviUlaznogNiza = uniformniZnakoviUlaznogNiza;
 	}
 
-	public void pomakni(String uniformniZnak, int stanje, int brojRetka,
-			String leksickaJedinka){
-		Cvor noviCvor = new Cvor(stanje, uniformniZnak, brojRetka, 
-				leksickaJedinka, false);
+	public void pomakni(String uniformniZnak, int stanje, int brojRetka, String leksickaJedinka) {
+		Cvor noviCvor = new Cvor(stanje, uniformniZnak, brojRetka, leksickaJedinka, false);
 		stog.push(noviCvor);
 	}
-	
-	public void reduciraj(int indeksProdukcije, int sljedeceStanje){
+
+	public void reduciraj(int indeksProdukcije) {
 		String lijeviZnakProdukcije = null;
 		List<String> desnaStranaProdukcije = null;
-		
-		for(Map.Entry<String, Map<Integer, List<String>>> produkcija : produkcije.entrySet()){
-			if(produkcija.getValue().containsKey(indeksProdukcije)){
+		int stanjeNaVrhuStoga;
+		int sljedeceStanje;
+		for (Map.Entry<String, Map<Integer, List<String>>> produkcija : produkcije.entrySet()) {
+			if (produkcija.getValue().containsKey(indeksProdukcije)) {
 				lijeviZnakProdukcije = produkcija.getKey();
 				desnaStranaProdukcije = produkcija.getValue().get(indeksProdukcije);
 				break;
 			}
 		}
-		
-		if(desnaStranaProdukcije.contains("$")){
+
+		if (desnaStranaProdukcije.contains("$")) {
+			if (stog.isEmpty()) {
+				stanjeNaVrhuStoga = 0;
+			} else {
+				stanjeNaVrhuStoga = stog.peek().getStanje();
+			}
+			sljedeceStanje = tablicaNovoStanje[stanjeNaVrhuStoga][nezavrsni.indexOf(lijeviZnakProdukcije)];
 			Cvor noviCvor = new Cvor(sljedeceStanje, lijeviZnakProdukcije, true);
 			noviCvor.dodajDijete(new Cvor(-1, "$", true));
 			stog.push(noviCvor);
-		}
-		else{
+		} else {
+			//djecu novog cvora treba dodavati u obrnutom redoslijedu od onog kako su poredani na vrhu stoga gledajuci od gore prema dolje
+			List<Cvor> skinutiZnakoviDesneStraneProdukcije = new ArrayList<>();
+			for (int i = 0; i < desnaStranaProdukcije.size(); i++) {
+				skinutiZnakoviDesneStraneProdukcije.add(stog.pop());
+			}
+			if (stog.isEmpty()) {
+				stanjeNaVrhuStoga = 0;
+			} else {
+				stanjeNaVrhuStoga = stog.peek().getStanje();
+			}
+			sljedeceStanje = tablicaNovoStanje[stanjeNaVrhuStoga][nezavrsni.indexOf(lijeviZnakProdukcije)];
 			Cvor noviCvor = new Cvor(sljedeceStanje, lijeviZnakProdukcije, true);
-			for(int i = 0; i < desnaStranaProdukcije.size(); i++){
-				noviCvor.dodajDijete(stog.pop());
+			for (int i = desnaStranaProdukcije.size() - 1; i >= 0; i--) {
+				noviCvor.dodajDijete(skinutiZnakoviDesneStraneProdukcije.get(i));
 			}
 			stog.push(noviCvor);
 		}
 	}
-	
-	public void prihvati(){
-		this.reduciraj(1, 0);
+
+	public void prihvati() {
+		this.reduciraj(1);
 		this.korijen = stog.pop();
 	}
-	
-	static class Cvor{
-		
+
+	private void izgradiStablo() {
+		int indeksUniformnogZnakaUTabliciAkcija;
+		int stanjeNaVrhuStoga;
+		ZapisTabliceUniformnihZnakova uniformniZnak;
+		Cvor vrhStoga;
+		Par akcija;
+		for (int i = 0, n = uniformniZnakoviUlaznogNiza.size(); i < n; i++) {
+			if (stog.isEmpty()) {
+				stanjeNaVrhuStoga = 0;
+			} else {
+				vrhStoga = stog.peek();
+				stanjeNaVrhuStoga = vrhStoga.stanje;
+			}
+			uniformniZnak = uniformniZnakoviUlaznogNiza.get(i);
+			indeksUniformnogZnakaUTabliciAkcija = zavrsni.indexOf(uniformniZnak.getUniformniZnak());
+			akcija = tablicaAkcija[stanjeNaVrhuStoga][indeksUniformnogZnakaUTabliciAkcija];
+			if (akcija.getAkcija() == AkcijaParsera.POMAKNI) {
+				pomakni(uniformniZnak.getUniformniZnak(), akcija.getIndex(), uniformniZnak.getRedak(),
+						uniformniZnak.getLeksickaJedinka());
+			} else if (akcija.getAkcija() == AkcijaParsera.REDUCIRAJ) {
+				reduciraj(akcija.getIndex());
+				//kazaljka treba ostati na istom mjestu u ulaznom nizu pa se smanjuje za 1 jer cu ju petlja povecati za 1
+				i--;
+			} else if (akcija.getAkcija() == AkcijaParsera.PRIHVATI) {
+				prihvati();
+			} else {
+				// akcija == ODBACI
+			}
+		}
+	}
+
+	static class Cvor {
+
 		private int stanje;
 		private String uniformniZnak;
 		private int brojRetka;
 		private String leksickaJedinka;
-		private boolean ispisatiSamoUniformniZnak; //da znam je li unutrasnji cvor ili list
-		
+		private boolean ispisatiSamoUniformniZnak; // da znam je li unutrasnji
+													// cvor ili list
+
 		private List<Cvor> djeca;
-		//kod ispisa paziti je li unutrasnji cvor ili list i je li $
-		public Cvor(int stanje, String uniformniZnak, int brojRetka, String leksickaJedinka, 
+
+		// kod ispisa paziti je li unutrasnji cvor ili list i je li $
+		public Cvor(int stanje, String uniformniZnak, int brojRetka, String leksickaJedinka,
 				boolean ispisatiSamoUniformniZnak) {
 			this.stanje = stanje;
 			this.uniformniZnak = uniformniZnak;
@@ -83,13 +136,14 @@ public class GenerativnoStablo {
 			this.leksickaJedinka = leksickaJedinka;
 			this.ispisatiSamoUniformniZnak = ispisatiSamoUniformniZnak;
 		}
-		
-		public Cvor(int stanje, String uniformniZnak, boolean jeNezavrsniZnak){
+
+		public Cvor(int stanje, String uniformniZnak, boolean jeNezavrsniZnak) {
 			this(stanje, uniformniZnak, -1, null, jeNezavrsniZnak);
 		}
-		
-		public void dodajDijete(Cvor dijete){
-			if(djeca == null) djeca = new LinkedList<>();
+
+		public void dodajDijete(Cvor dijete) {
+			if (djeca == null)
+				djeca = new LinkedList<>();
 			djeca.add(dijete);
 		}
 
@@ -120,11 +174,12 @@ public class GenerativnoStablo {
 		public void isIspisatiSamoUniformniZnak(boolean jeNezavrsniZnak) {
 			this.ispisatiSamoUniformniZnak = jeNezavrsniZnak;
 		}
-		
+
 		@Override
 		public String toString() {
-			if(ispisatiSamoUniformniZnak) return uniformniZnak;
-			else{
+			if (ispisatiSamoUniformniZnak)
+				return uniformniZnak;
+			else {
 				StringBuilder sb = new StringBuilder();
 				sb.append(uniformniZnak);
 				sb.append(" ");
--- PPJ_lab2/src/main/java/hr/fer/ppj/labos/lab2/SA.java
+++ PPJ_lab2/src/main/java/hr/fer/ppj/labos/lab2/SA.java
@@ -9,15 +9,29 @@ import hr.fer.ppj.labos.lab2.NovoStanje;
 
 public class SA {
 	public static void main(String[] args) {
-		 Akcija tablicaAkcija = null;
-		 NovoStanje tablicaNovoStanje = null;
-	        try (ObjectInputStream akcijaIn = new ObjectInputStream(new FileInputStream(GSA.AKCIJA_PATH));
-	        		ObjectInputStream novoStanjeIn = new ObjectInputStream(new FileInputStream(GSA.NOVO_STANJE_PATH))){
-	            tablicaAkcija = (Akcija) akcijaIn.readObject();
-	            tablicaNovoStanje = (NovoStanje) novoStanjeIn.readObject();
-	        }catch(Exception i) {
-	            i.printStackTrace();
-	        }
+		Akcija tablicaAkcija = null;
+		NovoStanje tablicaNovoStanje = null;
+		try (ObjectInputStream akcijaIn = new ObjectInputStream(new FileInputStream(GSA.AKCIJA_PATH));
+				ObjectInputStream novoStanjeIn = new ObjectInputStream(new FileInputStream(GSA.NOVO_STANJE_PATH))) {
+			tablicaAkcija = (Akcija) akcijaIn.readObject();
+			tablicaNovoStanje = (NovoStanje) novoStanjeIn.readObject();
+		} catch (Exception i) {
+			i.printStackTrace();
+		}
+
+		ParserTabliceUniformnihZnakova parser = new ParserTabliceUniformnihZnakova(args[0]); // TODO
+																								// u
+																								// eclipseu
+																								// podesi
+																								// ulazni
+																								// argument-put
+																								// do
+																								// datoteke
+																								// s
+																								// uniformnim
+																								// znakovima
+		GenerativnoStablo stablo = new GenerativnoStablo(tablicaAkcija, tablicaNovoStanje,
+				parser.getUniformniZnakoviUlaznogNiza());
 
 	}
 }
